5463c6b20cf8e6a65e21e01a7d9f74f63e586796
pcrglennon <pcrglennon@gmail.com>
2014-08-28 18:33:34 -0400

    Basic validity tests for property.  Geocoding tests should be moved to a shared behavior

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,3 +1,7 @@
 class Property < ActiveRecord::Base
   belongs_to :city
+
+  validates_presence_of :address, :latitude, :longitude
+  geocoded_by :address
+  before_validation :geocode
 end
--- spec/models/property_spec.rb
+++ spec/models/property_spec.rb
@@ -1,5 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe Property, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  let(:property) { FactoryGirl.create :property }
+
+  it 'has a valid factory' do
+    expect(property).to be_valid
+  end
+
+  it 'is invalid without a address' do
+    property = Property.new
+    expect(property).to_not be_valid
+    expect(property.errors.messages.keys).to include(:address)
+  end
+
+  it 'is invalid without a latitude' do
+    property = Property.new
+    expect(property).to_not be_valid
+    expect(property.errors.messages.keys).to include(:latitude)
+  end
+
+  it 'is invalid without a longitude' do
+    property = Property.new
+    expect(property).to_not be_valid
+    expect(property.errors.messages.keys).to include(:longitude)
+  end
+
+  it 'automatically creates a latitude/longitude from the name' do
+    expect(property.latitude).to_not be_nil
+    expect(property.longitude).to_not be_nil
+  end
+
 end

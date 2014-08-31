ca84c8f8050bf56939f7e1cf809f35fffd9d3601
pcrglennon <pcrglennon@gmail.com>
2014-08-31 16:06:02 -0400

    Home#listings returns only current/upcoming listings, Home#destroy destroys associated listings as well

--- app/models/home.rb
+++ app/models/home.rb
@@ -1,6 +1,10 @@
 class Home < ActiveRecord::Base
   belongs_to :city
-  has_many :listings
+  has_many :listings, :dependent => :destroy
 
   validates_presence_of :address
+
+  def listings
+    Listing.where(home_id: self.id).where("listings.end_date > ?", Date.today)
+  end
 end
--- spec/models/home_spec.rb
+++ spec/models/home_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Home, :type => :model do
 
-  let(:home) { FactoryGirl.create :home }
+  let(:home) { FactoryGirl.create :home_with_listings }
 
   it 'has a valid factory' do
     expect(home).to be_valid
@@ -12,6 +12,23 @@ RSpec.describe Home, :type => :model do
     home = Home.new
     expect(home).to_not be_valid
     expect(home.errors.messages.keys).to include(:address)
+  end
+
+  describe '#listings' do
+    it 'returns a list of all available listings' do
+      expect(home.listings).to_not be_empty
+    end
+
+    it 'only returns a list of current/upcoming listings' do
+      expect(home.listings.all? { |l| l.end_date > Date.today }).to be true
+    end
+  end
+
+  describe '#destroy' do
+    it 'destroys all associated listings as well' do
+      home.destroy
+      expect(Listing.where(home_id: home.id)).to be_empty
+    end
   end
 
 end

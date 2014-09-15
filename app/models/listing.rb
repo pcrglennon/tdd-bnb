bce9843d3152db2534fa1e214771ad8612a89c0e
pcrglennon <pcrglennon@gmail.com>
2014-09-15 14:36:58 -0400

    Validation specs for listing.home, listing.start_date, listing.end_date, listing.city

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,4 +1,10 @@
 class Listing < ActiveRecord::Base
   belongs_to :home
   belongs_to :city
+
+  validates_presence_of :home, :start_date, :end_date
+
+  def city
+    self.home.city
+  end
 end
--- spec/models/listing_spec.rb
+++ spec/models/listing_spec.rb
@@ -2,4 +2,35 @@ require 'rails_helper'
 
 RSpec.describe Listing, :type => :model do
 
+  let (:listing) { FactoryGirl.create :listing }
+
+  it 'has a valid factory' do
+    expect(listing).to be_valid
+  end
+
+  it 'is associated with a city' do
+    expect(listing.city).to_not be_nil
+  end
+
+  describe 'validations' do
+
+    let(:empty_listing) { Listing.new }
+
+    it 'is invalid without an associated home' do
+      expect(empty_listing).to_not be_valid
+      expect(empty_listing.errors.messages.keys).to include(:home)
+    end
+
+    it 'is invalid without a start date' do
+      expect(empty_listing).to_not be_valid
+      expect(empty_listing.errors.messages.keys).to include(:start_date)
+    end
+
+    it 'is invalid without a end date' do
+      expect(empty_listing).to_not be_valid
+      expect(empty_listing.errors.messages.keys).to include(:end_date)
+    end
+
+  end
+
 end
